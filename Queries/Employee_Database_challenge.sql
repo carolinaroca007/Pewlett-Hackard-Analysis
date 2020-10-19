2ac640e3beac59dd9dc1078b673599500bcc0eed
Carolina Roca <carolina.roca007@outlook.com>
2020-10-18 22:21:03 -0500

    Upload queries and resources.

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,57 @@
+--Create retirement titles table
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	t.title,
+	t.from_date,
+	t.to_date
+INTO retirement_titles
+FROM employees as e
+JOIN titles as t
+ON e.emp_no=t.emp_no
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31'
+ORDER BY emp_no;
+
+--View retirements titles table
+SELECT *
+FROM retirement_titles;
+
+--Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no)
+		emp_no,
+		first_name,
+		last_name,
+		title,
+		from_date,
+		to_date	
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no, to_date DESC;
+
+--create table of count of employees retiring by title
+SELECT  COUNT(title),
+	title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY (count(title)) DESC;
+
+--create a Mentorship Eligibility table for mentorship program
+SELECT DISTINCT ON (emp_no)
+	e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+INTO mentorship_eligibility
+FROM employees as e
+	JOIN dept_emp as de
+		ON e.emp_no=de.emp_no
+	JOIN titles as ti
+		ON e.emp_no=ti.emp_no
+WHERE de.to_date = ('9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no;
+
